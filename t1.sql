8a4187bdeb54e52cc7f46ac8ac2cff58546f4213
thasnirashid <thasnisaleem1999@gmail.com>
2022-05-24 12:17:59 +0530

    commit

--- t1.sql
+++ t1.sql
@@ -1,5 +1,5 @@
 set serveroutput on
-create or replace trigger display_salary_changes
+create or replace trigger disp_salary_changes
 before delete or insert or update on customer
 for each row
 when(new.id>0)
@@ -10,19 +10,5 @@ begin
 	dbms_output.put_line('Old salary : '|| :old.salary);
 	dbms_output.put_line('New salary : '|| :new.salary);
 	dbms_output.put_line('salary difference: '||sal_diff);
-end;
-/
-
-declare
-	total_rows number(2);
-begin
-	update customer
-	set salary=salary+5000;
-	if sql%notfound then
-		dbms_output.put_line('no customers updates');
-	elsif sql%found then
-		total_rows:=sql%rowcount;
-		dbms_output.put_line(total_rows||'customer updated');
-	end if;
 end;
 /
